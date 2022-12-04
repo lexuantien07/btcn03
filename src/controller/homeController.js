2cc03f80f619654eb2027d11bd31d63d637f68c8
20120593 <20120593@student.hcmus.edu.vn>
2022-12-04 10:17:21 +0700

    import movies and casts data

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,17 +3,16 @@ import datajsoncasts from '../db/casts.json';
 import datajsonmovies from '../db/movies.json';
 
 let getHomePage = async (req, res) => {
-    await connection.query(`drop table if exists a`);
+    await connection.query(`drop table if exists casts`);
     console.log('check data ', datajsoncasts[0]);
-    await connection.query(`create table a (id varchar(255),
+    await connection.query(`create table casts (id varchar(255),
                                             image varchar(255),
-                                            nameText varchar(255),
                                             name varchar(255),
                                             birthDate varchar(255),
                                             gender varchar(255)
                                             )`);
     for (let i = 0; i < datajsoncasts.length; i++) {
-        await connection.query(`insert into a(id, image, nameText, name, birthDate, gender) values ('${datajsoncasts[i].id}', '${datajsoncasts[i].image}', '${datajsoncasts[i].legacyNameText}', '${datajsoncasts[i].name}', '${datajsoncasts[i].birthDate}', '${datajsoncasts[i].gender}')`);
+        await connection.query(`insert into casts(id, image, name, birthDate, gender) values ('${datajsoncasts[i].id}', '${datajsoncasts[i].image}', '${datajsoncasts[i].name}', '${datajsoncasts[i].birthDate}', '${datajsoncasts[i].gender}')`);
     }                                        
     // await connection.query(`insert into a(id, image, nameText, name, birthDate, gender) values ('${datajson[0].id}', '${datajson[0].id}', '${datajson[0].id}', '${datajson[0].id}', '${datajson[0].id}', '${datajson[0].id}')`);
     await connection.query(`drop table if exists movies`);
@@ -25,9 +24,9 @@ let getHomePage = async (req, res) => {
                                             topRank varchar(255),
                                             rating varchar(255),
                                             genres varchar(255),
-                                            reviews varchar(25555),
+                                            reviews TEXT[][],
                                             synopses varchar(25555),
-                                            casts TEXT[]
+                                            casts TEXT[][]
                                             )`);
     for (let i = 0; i < datajsonmovies.length; i++) {
         if (datajsonmovies[i].synopses == undefined) {
@@ -37,12 +36,29 @@ let getHomePage = async (req, res) => {
             datajsonmovies[i].reviews = '';
         }
         let castsdata = [];
-        await connection.query(`insert into movies(id, image, title, year, topRank, rating, genres, reviews, synopses, casts) values ('${datajsonmovies[i].id}', '${datajsonmovies[i].img}', '${datajsonmovies[i].title}', '${datajsonmovies[i].year}', '${datajsonmovies[i].topRank}', '${datajsonmovies[i].rating}', '${datajsonmovies[i].genres}', '${datajsonmovies[i].reviews[0]}', '${datajsonmovies[i].synopses}', ARRAY['${datajsonmovies[i].casts}'])`);
+        castsdata.push(`${datajsonmovies[i].casts[0].id}'`)
+        for (let j = 1; j < datajsonmovies[i].casts.length - 1; j++) {
+            castsdata.push(`'${datajsonmovies[i].casts[j].id}'`)
+        }
+        castsdata.push(`'${datajsonmovies[i].casts[datajsonmovies[i].casts.length - 1].id}`)
+        let reviewsdata = [];
+        if (datajsonmovies[i].reviews[0] == undefined) {
+            reviewsdata.push('');
+        } else {
+            reviewsdata.push(`${datajsonmovies[i].reviews[0].author}'`)
+            for (let j = 0; j < datajsonmovies[i].reviews.length - 1; j++) {
+                reviewsdata.push(`'${datajsonmovies[i].reviews[j].author}'`)
+            }
+            reviewsdata.push(`'${datajsonmovies[i].reviews[datajsonmovies[i].reviews.length - 1].author}`)
+        }
+        await connection.query(`insert into movies(id, image, title, year, topRank, rating, genres, reviews, synopses, casts) values ('${datajsonmovies[i].id}', '${datajsonmovies[i].img}', '${datajsonmovies[i].title}', '${datajsonmovies[i].year}', '${datajsonmovies[i].topRank}', '${datajsonmovies[i].rating}', '${datajsonmovies[i].genres}', ARRAY['${reviewsdata}'], '${datajsonmovies[i].synopses}', ARRAY['${castsdata}'])`);
+        
+        console.log('check data import ', reviewsdata);
     }
     console.log('check unde ', datajsonmovies[4].reviews[0]);
-    console.log('check data import ', datajsonmovies[0].casts.length);
     const rows = await connection.query(`select * from movies`);
-    // console.log('check data base ', rows[0].casts[0]);
+    console.log('check data base ', rows[4].casts[1]);
+    console.log('check data base ', rows[0].reviews[0]);
     res.render('home.handlebars');
 };
 
